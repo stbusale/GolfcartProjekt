cf6db26c97e62a77409242b33fe9433b51153d2e
stgrusam <stgrusam@bx.fallmerayer.it>
2024-04-08 11:14:44 +0200

    Update steuerung.php

--- steuerung.php
+++ steuerung.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Meine Seite mit Menüleiste und WASD Steuerung</title>
 <!-- Bootstrap 5 CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cv/X&Uivz" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="style.css" rel="stylesheet">
 
 <style>
@@ -21,24 +21,41 @@
     #game-container {
         width: 400px;
         height: 400px;
-        border: 1px solid black;
         display: flex;
         flex-wrap: wrap;
         margin: auto; /* Center the game container */
-        margin-top: 0px; /* Add space between menu and game */
+        margin-top: 10px; /* Add space between menu and game */
+        padding: 10px;
+        box-sizing: border-box; /* Include padding in width */
     }
     .key {
-        width: 33.33%; /* Each key takes one-third of the container width */
-        height: 33.33%; /* Each key takes one-third of the container height */
+        width: calc(33.33% - 20px); /* Each key takes one-third of the container width minus margin */
+        height: calc(33.33% - 20px); /* Each key takes one-third of the container height minus margin */
         box-sizing: border-box;
-        border: 1px solid black;
         display: flex;
         justify-content: center;
         align-items: center;
         font-size: 24px;
+        margin: 5px; /* Add margin to all keys */
     }
     .active {
         background: lightblue;
+    }
+    /* Manually position the keys */
+    #E,
+    #F {
+        margin-left: 5px;
+        margin-right: 5px;
+        width: calc(50% - 10px); /* Set width to half the container width minus margin */
+    }
+    #R,
+    #G {
+        margin-right: 5px;
+        width: calc(50% - 10px); /* Set width to half the container width minus margin */
+    }
+    /* Manually add space to WASD */
+    #W, #A, #S, #D {
+        margin-bottom: 10px; /* Add margin to bottom */
     }
 </style>
 </head>
@@ -50,17 +67,25 @@
     <div class="key btn btn-primary" id="A">Left</div>
     <div class="key btn btn-primary" id="S">Backward</div>
     <div class="key btn btn-primary" id="D">Right</div>
+    <div class="key btn btn-primary" id="E">Open</div>
+    <div class="key btn btn-primary" id="R">Close</div>
+    <div class="key btn btn-primary" id="F">Push</div>
+    <div class="key btn btn-primary" id="G">Pull</div>
 </div>
 
 <!-- Bootstrap 5 JS -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-GwEVjflRK1A8X2B+S0&FNYIv4uqABCI5s9zoWsYWddK+yQ6GLZ1czcG5vHQ+fgRJ" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-YXy2ovjEe&/JqORn7IVlX4paC/nKYjBTz9aOb0lSMT5WT7rVsbrCqFNn6tbJFf6P" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
 <script>
     const commands = {
         W: 'forward',
         A: 'left',
         S: 'backward',
-        D: 'right'
+        D: 'right',
+        E: 'open',
+        R: 'close',
+        F: 'push',
+        G: 'pull'
     };
 
     let lastKeyPressed = '';
